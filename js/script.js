62f943857357a3443e29bd0b476e9feb3aa27cae
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-05-09 22:11:32 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let formElement = document.querySelector(".js-form");
+let haveElement = document.querySelector(".js-currency__have");
+let wantElement = document.querySelector(".js-currency__want");
+let currencyElement = document.querySelector(".js-currency");
+
+
+
+formElement.addEventListener("input", () => {
+    let have = haveElement.value;
+    let currency = currencyElement.value;
+
+    wantElement.value = have / currency;
+wantElement.value = (have / currency).toFixed(2);
+    console.log(wantElement);
+})
+
