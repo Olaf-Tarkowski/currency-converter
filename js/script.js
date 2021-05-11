28959f6d35b472a7b779d6db80f694841aaf7ead
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-05-11 22:30:00 +0200

    refactoring js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-let formElement = document.querySelector(".js-form");
-let haveElement = document.querySelector(".js-currency__have");
-let wantElement = document.querySelector(".js-currency__want");
-let currencyElement = document.querySelector(".js-currency");
+{
+    const currencyConverter = () => {
+        const haveElement = document.querySelector(".js-currency__have");
+        const wantElement = document.querySelector(".js-currency__want");
+        const currencyElement = document.querySelector(".js-currency");
 
+        const have = haveElement.value;
+        const currency = currencyElement.value;
 
+        wantElement.value = have / currency;
+        wantElement.value = (have / currency).toFixed(2);
 
-formElement.addEventListener("input", () => {
-    let have = haveElement.value;
-    let currency = currencyElement.value;
+    };
 
-    wantElement.value = have / currency;
-    wantElement.value = (have / currency).toFixed(2);
-    console.log(wantElement);
-})
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
 
+        formElement.addEventListener("input", currencyConverter);
+    };
+    init();
+}
